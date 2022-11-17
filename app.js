1d8efc04e6462fbd0f8431db6807eeb86fb0370c
Alejandro Iñiguez <ilex_happy99@hotmail.com>
2022-11-16 21:39:37 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function moverPosicionRandom (elm) {
+    elm.style.position = 'absolute';
+    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random() * (window.innerWidth - elm.offsetHeight) + 'px;'
+} 
+
+let btnSi = document.getElementById('btnSi');
+let btnNo = document.getElementById('btnNo');
+let divModoSecso = document.getElementsByClassName('modoSecso')[0];
+
+btnNo.addEventListener('mouseenter', function(e) { moverPosicionRandom(e.target)});
+btnSi.addEventListener('click', function(e) {
+    alert('Sabía que dirias que sí. Te amo ❤')
+
+    divModoSecso.style.display = 'block';
+    const cancion = new Audio('./img/modo_hot.mp3');
+    cancion.play();
+});
